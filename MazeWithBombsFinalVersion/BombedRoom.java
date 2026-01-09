e5a4770249b91a562757d4d379fbaf83a3c2c032
Denys Tkachenko <109801716+Den1717171717@users.noreply.github.com>
2026-01-09 14:30:57 +0100

    Delete MazeWithBombsFinalVersion directory

--- MazeWithBombsFinalVersion/BombedRoom.java
+++
@@ -1,7 +0,0 @@
-package org.den.WP_2025_DT.lab4.MazeWithBombsFinalVersion;
-
-public class BombedRoom extends Room {
-    public BombedRoom(int x, int y, int roomNumber) {
-        super(x, y, roomNumber, true);
-    }
-}
