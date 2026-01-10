37e69dfb103570ed3548fd5af9a3b1d7c48c30be
Denys Tkachenko <109801716+Den1717171717@users.noreply.github.com>
2026-01-10 21:22:33 +0100

    FinalVersionCalculator

---
+++ FinalVersionCalculator/CalculatorModel.java
@@ -0,0 +1,28 @@
+enum Operation {
+    PLUS , MINUS, MULTIPLY, DIVIDE
+}
+public class CalculatorModel {
+    private double num;
+    private double answer;
+    private Operation operation;
+
+    public void setNum(double num) {
+        this.num = num;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public double calculate(double secondNum) {
+        switch (operation) {
+            case PLUS: answer = num + secondNum; break;
+            case MINUS: answer = num - secondNum; break;
+            case MULTIPLY: answer = num * secondNum; break;
+            case DIVIDE: answer = num / secondNum; break;
+            default: answer = 0;
+        }
+        return answer;
+    }
+}
+
